6a9528ac6e42911f8c1dedb31b09832f344e54f3
PlayerG9 <g9yout@gmail.com>
2021-09-20 12:26:45 +0200

    added test command and argument input

--- commandapp/__main__.py
+++ commandapp/__main__.py
@@ -1,8 +1,18 @@
+import sys
+import shlex
+if len(sys.argv) <= 1:
+    sys.argv.extend(shlex.split(input("=> ")))
 import commandapp as cmdapp
 
 
 app = cmdapp.CommandApp()
 
 app.version = cmdapp.__version__
+
+
+@app.register
+def test():
+    raise ValueError()
+
 
 app.run()
