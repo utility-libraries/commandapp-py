0b32928ebd827e838a39250633365e3e6d80c634
PlayerG9 <g9yout@gmail.com>
2021-09-20 11:49:18 +0200

    added header and error-classes import

--- commandapp/__init__.py
+++ commandapp/__init__.py
@@ -2,11 +2,20 @@
 r"""
 
 """
+from .errors import *
 exec(r"""
 import sys
 if sys.version_info < (3, 6):
-    raise RuntimeError("invalid python version")
+    raise PythonVersionError("invalid python version") from None
 """)  # check for python version without new variables
 from .cmdapp import CommandApp
 
-__version__ = (0,0,1)
+__author__ = "PlayerG9"
+__copyright__ = "Copyright 2021, PlayerG9"
+__credits__ = ["PlayerG9"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__version_info__ = (0,0,1)
+__maintainer__ = "PlayerG9"
+__email__ = None
+__status__ = "Prototype"  # Prototype, Development, Production
