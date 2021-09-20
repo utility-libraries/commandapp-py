0c8972e5797a92906276439879231698f78ca271
PlayerG9 <g9yout@gmail.com>
2021-09-20 11:26:50 +0200

    added python-version check and package version info

--- commandapp/__init__.py
+++ commandapp/__init__.py
@@ -2,4 +2,11 @@
 r"""
 
 """
+exec(r"""
+import sys
+if sys.version_info < (3, 6):
+    raise RuntimeError("invalid python version")
+""")  # check for python version without new variables
 from .cmdapp import CommandApp
+
+__version__ = (0,0,1)
