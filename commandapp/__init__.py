eac4aa98da8677da56a76f2469e8420cc6940ff4
PlayerG9 <g9yout@gmail.com>
2021-09-20 14:12:26 +0200

    added comment for later version

--- commandapp/__init__.py
+++ commandapp/__init__.py
@@ -3,7 +3,7 @@ r"""
 
 """
 from .errors import *
-exec(r"""
+exec(r"""  # maybe needs to get removed
 import sys
 if sys.version_info < (3, 6):
     raise PythonVersionError("invalid python version") from None
@@ -14,8 +14,8 @@ __author__ = "PlayerG9"
 __copyright__ = "Copyright 2021, PlayerG9"
 __credits__ = ["PlayerG9"]
 __license__ = "MIT"
-__version__ = "0.0.1"
-__version_info__ = (0,0,1)
+__version_info__ = (0,1,0)
+__version__ = '.'.join(str(_) for _ in __version_info__)
 __maintainer__ = "PlayerG9"
 __email__ = None
 __status__ = "Prototype"  # Prototype, Development, Production
