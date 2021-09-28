cdf776bcf8de5c5adf74add1b501e1855effe4a0
PlayerG9 <g9yout@gmail.com>
2021-09-28 16:18:33 +0200

    changed version from 0.2.0 to 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 __author__ = "PlayerG9"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 setuptools.setup(
     name="commandapp",
